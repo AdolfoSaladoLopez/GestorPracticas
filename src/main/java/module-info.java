fdfc7cf6714d267bcca1d2a56350f87931e8c16e
AdolfoSaladoLopez <adolfo.s255446@cesurformacion.com>
2022-11-19 09:24:11 +0100

    Modificadas dependencias en POM: - Sustitución Jakarta Persistence API por Javax Persistence. - Añadida Commons Code.
    Añadido:
    - Hibernate.cfg.xml con la configuración de la BBDD y Mappings.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.gestorpracticas {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens models;
 
     opens com.example.gestorpracticas to javafx.fxml;
     exports com.example.gestorpracticas;
